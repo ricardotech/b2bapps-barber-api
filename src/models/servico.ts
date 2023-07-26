19e3b632c8be7cb6835be3b3cd551809b28127d6
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-26 14:33:22 -0300

    fix: Sugestão de arquitetura de agendamento

--- src/models/servico.ts
+++ src/models/servico.ts
@@ -7,6 +7,14 @@ interface ServicoModel extends Document {
   barbearia: Types.ObjectId;
   preco: number;
   duracao_minutos: number;
+  /*
+  Quantidade de minutos necessários para realizar a manutenção do serviço.
+  Caso o tempo de execução do serviço seja maior que 30 minutos o valor do buffer terá como padrão 0 minutos,
+  caso contrário o valor do buffer será a diferença entre 30 minutos e o tempo de execução do serviço.
+  Possui como objetivo fornecer um tempo de folga para o barbeiro entre um serviço e outro.
+  O buffer poderá ser utilizado para realocar um agendamento, essa realocação poderá ser feita apenas pelo barbeiro.
+  */
+  buffer?: number;
 }
 
 const servicoSchema = new Schema<ServicoModel>({
