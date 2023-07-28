9adf21805ff68dcaaf3a526c3816ba1ea9695a0e
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-28 13:05:52 -0300

    fix: Implementação das funções de adição e deletação do gestor

--- src/routes/b2bapps/gestor.ts
+++ src/routes/b2bapps/gestor.ts
@@ -4,6 +4,8 @@ import {
   getGestorByCpf,
 } from "../../controllers/b2bapps/gestor/get";
 import { getBarberShopByGestor, getGestorById } from "../../utils/gestor/get";
+import { createGestor } from "../../utils/gestor/post";
+import { deleteGestor } from "../../utils/gestor/delete";
 
 const b2b_gestorRouter = Router();
 
@@ -11,5 +13,7 @@ b2b_gestorRouter.get("/", getAllGestores); // Rota para listar todos os gestores
 b2b_gestorRouter.get("/:id", getGestorById); // Rota para listar um gestor pelo id
 b2b_gestorRouter.get("/cpf/:cpf", getGestorByCpf); // Rota para listar um gestor pelo cpf
 b2b_gestorRouter.get("/:id/barbearia", getBarberShopByGestor); // Rota para listar todas as barbearias pelo id do gestor
+b2b_gestorRouter.post("/", createGestor); // Rota para criar um gestor
+b2b_gestorRouter.delete("/:id", deleteGestor); // Rota para deletar um gestor
 
 export default b2b_gestorRouter;
--- src/routes/gestor.ts
+++ src/routes/gestor.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getBarberShopByGestor, getGestorById } from "../utils/gestor/get";
+import { createGestor } from "../utils/gestor/post";
+import { deleteGestor } from "../utils/gestor/delete";
 
 const gestorRouter = Router();
 
-gestorRouter.get("/:id", getGestorById);
-gestorRouter.get("/:id/barbearia", getBarberShopByGestor);
+gestorRouter.get("/:id", getGestorById); // Rota para listar um gestor pelo id
+gestorRouter.get("/:id/barbearia", getBarberShopByGestor); // Rota para listar todas as barbearias pelo id do gestor
+gestorRouter.post("/", createGestor); // Rota para criar um gestor
+gestorRouter.delete("/:id", deleteGestor); // Rota para deletar um gestor
 
 export default gestorRouter;
