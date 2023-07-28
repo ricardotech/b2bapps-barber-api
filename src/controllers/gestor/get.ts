7fcf280c1a0b31729f07209e933898b2495a1c11
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-28 00:39:37 -0300

    feat: Listagem de todas as barbearias de um gestor

--- src/controllers/gestor/get.ts
+++ src/controllers/gestor/get.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Gestor from "../../models/gestor";
+import Barbearia from "../../models/barbearia";
 
 interface Prop {
   req: Request;
@@ -13,6 +14,21 @@ export async function getGestorById({ req, res }: Prop) {
       return res.status(404).json({ mensagem: "Gestor não encontrado" });
     }
     return res.status(200).json(gestor);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+}
+
+export async function getBarberShopByGestor({ req, res }: Prop) {
+  try {
+    const { id } = req.params;
+    const gestor = await Gestor.findById(id);
+    if (!gestor) {
+      return res.status(404).json({ mensagem: "Gestor não encontrado" });
+    }
+    const barbearias = await Barbearia.find({ _id_gestor: id });
+    return res.status(200).json(barbearias);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
--- src/routes/gestor.ts
+++ src/routes/gestor.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getGestorById } from "../controllers/gestor/get";
+import {
+  getBarberShopByGestor,
+  getGestorById,
+} from "../controllers/gestor/get";
 
 const gestorRouter = Router();
 
-gestorRouter.get("/:id",getGestorById)
+gestorRouter.get("/:id", getGestorById);
+gestorRouter.get("/:id/barbearia", getBarberShopByGestor);
 
 export default gestorRouter;
