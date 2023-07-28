d3699e13dc425cf2ce3d4e219d6546ba1d5964fb
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-28 13:04:48 -0300

    feat: Função para criar um gestor

---
+++ src/utils/gestor/post.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { GestorType } from "../../types";
+import Gestor from "../../models/gestor";
+
+interface Prop {
+  req: Request;
+  res: Response;
+}
+
+export async function createGestor({ req, res }: Prop) {
+  try {
+    const body: GestorType = req.body;
+    const { nome, telefone, email, documento } = body;
+    if (!nome || !telefone || !email || !documento) {
+      return res
+        .status(400)
+        .json({ message: "Dados obrigatórios não informados" });
+    }
+    if (telefone.length !== 11) {
+      return res.status(400).json({ message: "Telefone inválido" });
+    }
+    if (!email.includes("@") || !email.includes(".") || email.length > 320) {
+      return res.status(400).json({ message: "Email inválido" });
+    }
+    if (!documento.cpf || !documento.naturalidade || !documento.filiacao) {
+      return res.status(400).json({ message: "Documento inválido" });
+    }
+    if (documento.cpf.length !== 11) {
+      return res.status(400).json({ message: "CPF inválido" });
+    }
+    const gestor = await Gestor.create(body);
+    return res.status(201).json(gestor);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
+}
