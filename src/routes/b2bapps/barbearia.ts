cd7a8f26e1253b7b72c677ebc9a08bf217e55108
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-31 16:01:25 -0300

    feat: Atribuição das funções

--- src/routes/b2bapps/barbearia.ts
+++ src/routes/b2bapps/barbearia.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import verificaId from "../../middleware/verificaId";
 import { getAllBarberShop } from "../../controllers/b2bapps/barbearia/get";
 import { createBarberShop } from "../../utils/barbearia/post";
+import { deleteBarbearia } from "../../utils/barbearia/delete";
+import { updateStatusBarberShop } from "../../utils/barbearia/patch";
 
 const b2b_barbeariaRouter = Router();
 
 b2b_barbeariaRouter.get("/", getAllBarberShop); // Rota para listar todas as barbearias
 b2b_barbeariaRouter.get("/:id", verificaId, (req, res) => {}); // Rota para listar uma barbearia específica
 b2b_barbeariaRouter.post("/", createBarberShop); // Rota para criar uma barbearia
-b2b_barbeariaRouter.delete("/:id", verificaId, (req, res) => {}); // Rota para deletar uma barbearia
+b2b_barbeariaRouter.delete("/:id", verificaId, deleteBarbearia); // Rota para deletar uma barbearia
 b2b_barbeariaRouter.put("/:id", verificaId, (req, res) => {}); // Rota para atualizar uma barbearia
 b2b_barbeariaRouter.patch("/:id/plano", verificaId, (req, res) => {}); // Rota para atualizar o plano de uma barbearia
-b2b_barbeariaRouter.patch("/:id/status", verificaId, (req, res) => {}); // Rota para atualizar o status de uma barbearia
+b2b_barbeariaRouter.patch("/:id/status", verificaId, updateStatusBarberShop); // Rota para atualizar o status de uma barbearia
 
 export default b2b_barbeariaRouter;
