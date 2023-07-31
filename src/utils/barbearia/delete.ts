f3cebe345bbc75ce65d5f838dda656bd00cf866a
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-31 16:00:17 -0300

    feat: Função de deletar uma barbearia

---
+++ src/utils/barbearia/delete.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import Barbearia from "../../models/barbearia";
+
+export async function deleteBarbearia(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const { _id_gestor } = req.body;
+    const barbearia = await Barbearia.findById(id);
+    if (!barbearia) {
+      return res.status(404).json({ mensagem: "Barbearia não encontrada" });
+    }
+    if (barbearia.status) {
+      return res.status(400).json({ mensagem: "Barbearia ainda está ativada" });
+    }
+    if (barbearia._id_gestor.toString() !== _id_gestor) {
+      return res
+        .status(400)
+        .json({ mensagem: "Usuário não é gestor da barbearia" });
+    }
+    await Barbearia.findByIdAndDelete(id);
+    return res.status(200).json({ mensagem: "Barbearia deletada com sucesso" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+}
