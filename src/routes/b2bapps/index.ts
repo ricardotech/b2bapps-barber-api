2cae6d1301bbf231c0eef514bc500ee7fb1c1a6c
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-07-31 21:13:51 -0300

    feat: Adição da rota dos barbeiros

--- src/routes/b2bapps/index.ts
+++ src/routes/b2bapps/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import b2b_gestorRouter from "./gestor";
 import b2b_barbeariaRouter from "./barbearia";
+import b2b_barbeiroRouter from "./barbeiro";
 
 const b2bappsRouter = Router();
 
 b2bappsRouter.use("/gestor", b2b_gestorRouter);
 b2bappsRouter.use("/barbearia", b2b_barbeariaRouter);
+b2bappsRouter.use("/barbeiro", b2b_barbeiroRouter);
 
 export default b2bappsRouter;
