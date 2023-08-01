ae07c74bfdb6352e722648b62f68f0f83152b122
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-08-01 18:37:40 -0300

    feat: Função de reuperação de barbearia pelo id

--- src/controllers/b2bapps/barbearia/get.ts
+++ src/controllers/b2bapps/barbearia/get.ts
@@ -5,11 +5,23 @@ export async function getAllBarberShop(req: Request, res: Response) {
   try {
     await Barbearia.find().then((barbearias) => {
       return res.status(200).json(barbearias);
-    })
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
   }
 }
 
-export async function getBarberShopById(req: Request, res: Response) {}
+export async function getBarberShopById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const barbearia = await Barbearia.findById(id);
+    if (!barbearia) {
+      return res.status(404).json({ mensagem: "Barbearia não encontrada" });
+    }
+    return res.status(200).json(barbearia);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+}
